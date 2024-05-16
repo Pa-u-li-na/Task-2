f75d610b0efac17aa67ba5c3dcad66903aa85464
Pa-u-li-na <pvavaeva@edu.hse.ru>
2024-05-16 18:07:18 +0300

    Added tests

--- src/NumberAnalyzerTest.java
+++ src/NumberAnalyzerTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.FileWriter;
 import java.util.*;
@@ -9,35 +10,30 @@ import java.util.*;
 
 public class NumberAnalyzerTest {
 
-    // Тестирование нахождения минимума
     @Test
     public void testFindMin() {
         List<Integer> numbers = Arrays.asList(5, 2, 8, 1, 9);
         assertEquals(1, Main._min(numbers));
     }
 
-    // Тестировние нахождения максимума
     @Test
     public void testFindMax() {
         List<Integer> numbers = Arrays.asList(5, 2, 8, 1, 9);
         assertEquals(9, Main._max(numbers));
     }
 
-    // Тестирование суммирования
     @Test
     public void testSum() {
         List<Integer> numbers = Arrays.asList(5, 2, 8, 1, 9);
         assertEquals(25, Main._sum(numbers));
     }
 
-    // Тестирование произведения
     @Test
     public void testMultiply() {
         List<Integer> numbers = Arrays.asList(2, 3, 4);
         assertEquals(24, Main._mult(numbers));
     }
 
-    // Тестирование всех функций
     @Test
     public void testMinMaxSumMultInt() throws IOException {
         ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 4, 2, 3));
@@ -47,16 +43,15 @@ public class NumberAnalyzerTest {
         assertEquals(24, Main._mult(numbers));
     }
 
-    // Тестирование скорости при увеличении размера файла
     @Test
     public void testPerformanceSum() throws IOException {
         for (int size = 1; size <= 1000000; size *= 10) {
             createFileWithNumbers("numbers.txt", size);
             ArrayList<Integer> numbers = Main.readNumbersFromFile("numbers.txt");
-            long startTime = System.currentTimeMillis();
+            long startTime = System.nanoTime();
             Main._sum(numbers);
-            long endTime = System.currentTimeMillis();
-            long duration = (endTime - startTime);
+            long endTime = System.nanoTime();
+            double duration = (double) (endTime - startTime)/ 1000000;
             System.out.println("Размер файла: " + size + ", Время выполнения: " + duration + " мс");
         }
     }
@@ -69,7 +64,7 @@ public class NumberAnalyzerTest {
             long startTime = System.nanoTime();
             Main._min(numbers);
             long endTime = System.nanoTime();
-            long duration = (endTime - startTime) / 1000000; // milliseconds
+            double duration = (double) (endTime - startTime)/ 1000000;
             System.out.println("Размер файла: " + size + ", Время выполнения: " + duration + " мс");
         }
     }
@@ -82,7 +77,7 @@ public class NumberAnalyzerTest {
             long startTime = System.nanoTime();
             Main._max(numbers);
             long endTime = System.nanoTime();
-            long duration = (endTime - startTime) / 1000000; // milliseconds
+            double duration = (double) (endTime - startTime)/ 1000000;
             System.out.println("Размер файла: " + size + ", Время выполнения: " + duration + " мс");
         }
     }
@@ -95,25 +90,37 @@ public class NumberAnalyzerTest {
             long startTime = System.nanoTime();
             Main._mult(numbers);
             long endTime = System.nanoTime();
-            long duration = (endTime - startTime) / 1000000; // milliseconds
+            double duration = (double) (endTime - startTime)/ 1000000;
             System.out.println("Размер файла: " + size + ", Время выполнения: " + duration + " мс");
         }
     }
 
-    // Тестировние использования нецелых чисел для всех функций
     @Test
-    public void testMinMaxSumMultDouble() throws IOException {
-        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 4, 2, 3));
-        assertEquals(1.5, Main._min(numbers));
-        assertEquals(4.2, Main._max(numbers));
-        assertEquals(11.6, Main._sum(numbers));
-        assertEquals(49.932, Main._mult(numbers));
+    public void testNegativeNumbers() throws IOException {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, -9, 3, 5, -1));
+        assertEquals(-9, Main._min(numbers));
+        assertEquals(5, Main._max(numbers));
+        assertEquals(-1, Main._sum(numbers));
+        assertEquals(135, Main._mult(numbers));
+    }
+
+    @Test
+    public void testReadNumbersFromFile() throws IOException {
+        FileWriter fileWriter = new FileWriter("numbers.txt");
+        for (int i = 1; i < 6; i ++) {
+            fileWriter.write(i + " ");
+        }
+        fileWriter.close();
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> actual = Main.readNumbersFromFile("numbers.txt");
+        assertEquals(expected, actual);
     }
 
     private void createFileWithNumbers(String fileName, int size) throws IOException {
         FileWriter fileWriter = new FileWriter(fileName);
+        Random random = new Random();
         for (int i = 0; i < size; i++) {
-            fileWriter.write(i + " ");
+            fileWriter.write(random.nextInt(1000000) + " ");
         }
         fileWriter.close();
     }
